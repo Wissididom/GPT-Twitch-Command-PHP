a29157727436f9772452ac6bc7f82bfdb5441c2b
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-04-19 18:21:01 +0200

    Load `max_tokens` from env file

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (isset($_GET['prompt'])) {
 	]);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
 		'model' => 'gpt-3.5-turbo',
-		'max_tokens' => 180, // estimated by using https://platform.openai.com/tokenizer
+		'max_tokens' => $MAX_TOKENS,
 		'messages' => [
 			[
 				'role' => 'user',
