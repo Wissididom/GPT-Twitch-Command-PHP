7df0d9f0502620c6068bc68375c2768fd2198949
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-05-07 17:07:56 +0200

    Make processing text and response prefix configurable through query param

--- streamelements.php
+++ streamelements.php
@@ -5,7 +5,7 @@ if (isset($_GET['prompt'])) {
 	set_time_limit(0);
 	ob_start();
 	// Initial processing
-	echo 'Processing...';
+	echo isset($_GET['processing']) ? $_GET['processing'] : 'Processing...';
 	header('Connection: close');
 	header('Content-Length: ' . ob_get_length());
 	ob_end_flush();
@@ -40,7 +40,7 @@ if (isset($_GET['prompt'])) {
 		'Content-Type: application/json'
 	]);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
-		'message' => '🤖' . substr($response['choices'][0]['message']['content'], 0, 440) // StreamElements seems to prevent messages of more than 500 chars. It seems like the limit is around 450
+		'message' => (isset($_GET['prefix']) ? $_GET['prefix'] : '🤖') . substr($response['choices'][0]['message']['content'], 0, 440) // StreamElements seems to prevent messages of more than 500 chars. It seems like the limit is around 450
 	]));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$response = json_decode(curl_exec($ch), true);
